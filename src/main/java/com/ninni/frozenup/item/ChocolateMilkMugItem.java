fa6ac9ebc35edf3b6b544ea40d435cbd32748300
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-08-12 15:33:35 +0200

    tags and mug fix

--- src/main/java/com/ninni/frozenup/item/ChocolateMilkMugItem.java
+++ src/main/java/com/ninni/frozenup/item/ChocolateMilkMugItem.java
@@ -7,16 +7,16 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
-public class ChocolateMilkMugItem extends MilkMugItem {
+public class ChocolateMilkMugItem extends AbstractDrinkableMugItem {
     public ChocolateMilkMugItem(Block block, Settings settings) { super(block, settings); }
 
     @Override
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        user.clearStatusEffects();
         if (!world.isClient) {
             user.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 0));
             user.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600, 1));
         }
-
         return super.finishUsing(stack, world, user);
     }
 }
