1e93aa56bcd856a0a6d69bb26679c834ce93b9a4
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-08-17 02:13:18 +0200

    Mug drinking sounds

--- src/main/java/com/ninni/frozenup/item/AbstractDrinkableMugItem.java
+++ src/main/java/com/ninni/frozenup/item/AbstractDrinkableMugItem.java
@@ -1,11 +1,14 @@
 package com.ninni.frozenup.item;
 
+import com.ninni.frozenup.sound.FrozenUpSoundEvents;
 import net.minecraft.block.Block;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsage;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
@@ -27,6 +30,16 @@ public abstract class AbstractDrinkableMugItem extends BlockItem {
     @Override public int getMaxUseTime(ItemStack stack) { return 32; }
 
     @Override public UseAction getUseAction(ItemStack stack) { return UseAction.DRINK; }
+
+    @Override
+    public SoundEvent getDrinkSound() {
+        return FrozenUpSoundEvents.ITEM_MUG_DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatSound() {
+        return FrozenUpSoundEvents.ITEM_MUG_DRINK;
+    }
 
     @Override public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) { return ItemUsage.consumeHeldItem(world, user, hand); }
 }
--- src/main/java/com/ninni/frozenup/sound/FrozenUpSoundEvents.java
+++ src/main/java/com/ninni/frozenup/sound/FrozenUpSoundEvents.java
@@ -23,8 +23,12 @@ public class FrozenUpSoundEvents {
     public static final SoundEvent BLOCK_CHILLOO_FEATHER_LAMP_TOGGLE_OFF = chillooFeatherLamp("toggle_off");
     private static SoundEvent chillooFeatherLamp(String type) { return block("chilloo_feather_lamp", type); }
 
+    public static final SoundEvent ITEM_MUG_DRINK = mug("drink");
+    private static SoundEvent mug(String type) { return item("mug", type); }
+
     private static SoundEvent block(String block, String type) { return register("block." + block + "." + type); }
     private static SoundEvent entity(String entity, String type) { return register("entity." + entity + "." + type); }
+    private static SoundEvent item(String item, String type) { return register("item." + item + "." + type); }
 
     private static SoundEvent register(String name) {
         Registry.register(Registry.SOUND_EVENT, new Identifier(MOD_ID, name), new SoundEvent(new Identifier(MOD_ID, name)));
