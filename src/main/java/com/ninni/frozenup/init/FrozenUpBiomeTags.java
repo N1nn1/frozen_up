3485f589be6963c86a1d2125455e27c56955eca1
0rc1nus <mitchellctlui@gmail.com>
2022-09-17 15:31:53 +0800

    Removed conventional tags + added biome modifiers back

---
+++ src/main/java/com/ninni/frozenup/init/FrozenUpBiomeTags.java
@@ -0,0 +1,19 @@
+package com.ninni.frozenup.init;
+
+import com.ninni.frozenup.FrozenUp;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
+
+public class FrozenUpBiomeTags {
+
+    public static final TagKey<Biome> PENGUIN_SPAWNS_IN = create("penguin_spawns_in");
+    public static final TagKey<Biome> REINDEER_SPAWNS_IN = create("reindeer_spawns_in");
+    public static final TagKey<Biome> CHILLOO = create("chilloo_spawns_in");
+
+    private static TagKey<Biome> create(String id) {
+        return TagKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(FrozenUp.MOD_ID, id));
+    }
+
+}
